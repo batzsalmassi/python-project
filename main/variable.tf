5d5cbf45b83351e2c4c4fe97ac54bf1e4b615014
seanPrivate <sean.salmassi1@gmail.com>
2024-07-30 15:15:05 +0300

    test commit

---
+++ main/variable.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+    description = "AWS region"  # Description of the variable
+    type        = string        # Type of the variable
+    default     = "us-east-1"   # Default value for the variable
+}
