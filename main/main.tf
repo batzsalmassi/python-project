b96b5493b8aa02795e77162c33f059b52c33c5da
seanPrivate <sean.salmassi1@gmail.com>
2024-07-30 19:09:58 +0300

    test

--- main/main.tf
+++ main/main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
     region = var.aws_region  # Specify the AWS region to use
 }
+
+resource "vpc" "main" {
+    cidr_block = var.vpc_cidr_block  # Specify the CIDR block for the VPC
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    tags = {
+        Name = var.vpc_name  # Specify the name of the VPC
+    }
+}
--- main/variable.tf
+++ main/variable.tf
@@ -3,3 +3,9 @@ variable "aws_region" {
     type        = string        # Type of the variable
     default     = "us-east-1"   # Default value for the variable
 }
+
+variable "vpc_cidr_block" {
+    description = "CIDR block for the VPC"  # Description of the variable
+    type        = string                   # Type of the variable
+    default     = "122.0.0.0/16"       # Default value for the variable
+}
