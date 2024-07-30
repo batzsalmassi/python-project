cfd13eb0df8850921c6a203357ca9b766e1310d2
seanPrivate <sean.salmassi1@gmail.com>
2024-07-30 19:31:12 +0300

    format terraform files

--- main/variable.tf
+++ main/variable.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
-    description = "AWS region"  # Description of the variable
-    type        = string        # Type of the variable
-    default     = "us-east-1"   # Default value for the variable
+  description = "AWS region" # Description of the variable
+  type        = string       # Type of the variable
+  default     = "us-east-1"  # Default value for the variable
 }
 
 variable "vpc_cidr_block" {
-    description = "CIDR block for the VPC"  # Description of the variable
-    type        = string                   # Type of the variable
-    default     = "122.0.0.0/16"       # Default value for the variable
+  description = "CIDR block for the VPC" # Description of the variable
+  type        = string                   # Type of the variable
+  default     = "122.0.0.0/16"           # Default value for the variable
 }
