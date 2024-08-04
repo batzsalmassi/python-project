2b45ecc49cf8811ffef59c8d919201811cbc8e41
batzsalmassi <sean.salmassi1@gmail.com>
2024-08-04 13:25:11 +0300

    test

--- main/main.tf
+++ main/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
-  enable_dns_support   = false
+  enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
     Name = "main-vpc"
