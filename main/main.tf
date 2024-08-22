c164a766b9d4a55c9284d6f9f7dcb34dcc20621b
batzsalmassi <sean.salmassi1@gmail.com>
2024-08-22 16:14:02 +0300

    Fix VPC and public/private key creation

--- main/main.tf
+++ main/main.tf
@@ -1,26 +1,79 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.0.0" # Specify the version constraint as needed
-    }
+
+#---------------modules-------------------#
+# create vpc
+
+module "vpc" {
+    source = "terraform-aws-modules/vpc/aws"
+    version = "3.2.0"
+
+    name                 = "py-prod-proj-vpc"
+    cidr                 = var.vpc_cidr
+    azs                  = var.azs
+    private_subnets      = var.private_subnets
+    public_subnets       = var.public_subnets
+    enable_nat_gateway   = false
+    enable_vpn_gateway   = false
+    enable_dns_hostnames = true
+    enable_dns_support = true
+ 
+}
+#---------------security_group-------------------#
+
+
+
+resource "aws_security_group" "Allow_services" {
+  name = "PROD"
+  description = "global rule"
+  vpc_id = 	module.vpc.vpc_id
+  
+  ingress {
+    description = "All traffic_in"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "traffic in HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"] # access from any
+  }
+
+  ingress {
+    description = "traffic in HTTP"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"] # access from any
+  }
+
+  egress {
+    description = "All traffic_out"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-provider "aws" {
-  region = var.aws_region
+
+#---------------key_pair-------------------#
+# create key_pair with private key and public_key
+
+resource "aws_key_pair" "TF-key" {
+  key_name   = "TF-key"
+  public_key = tls_private_key.rsa.public_key_openssh
 }
 
-module "vpc" {
-  source = "./modules/vpc"
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
-  name                 = var.name
-  cidr                 = var.cidr
-  enable_dns_support   = var.enable_dns_support
-  enable_dns_hostnames = var.enable_dns_hostnames
-  public_subnet_count  = var.public_subnet_count
-  private_subnet_count = var.private_subnet_count
-
-  tags = var.tags
-
+resource "local_file" "TF-key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "/Users/sean.salmassi/github-Repos/python-project/TF-key"
 }
