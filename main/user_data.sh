dc3dd393d9f528334d15e9d766dec49dfcb21ded
Sean Salmassi <167541223+batzsalmassi@users.noreply.github.com>
2024-08-23 17:45:34 +0300

    Update user_data.sh

--- main/user_data.sh
+++ main/user_data.sh
@@ -7,7 +7,6 @@ systemctl enable docker
 usermod -aG docker ec2-user
 export SHODAN_API_KEY=${{ secrets.SHODAN_API_KEY }}
 
-
 # pull the most recent image from docker hub.
 docker pull seansal/shodan_app:$(curl -s https://hub.docker.com/v2/repositories/seansal/shodan_app/tags/?page_size=1 | grep -oP '"name":\s*"\K[^"]+')
 
