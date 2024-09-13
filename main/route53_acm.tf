8b31637c0a75646e5b1be7db77c6618a6757d2cf
batzsalmassi <sean.salmassi1@gmail.com>
2024-09-13 10:54:04 +0300

    fix route53 dns valid

--- main/route53_acm.tf
+++ main/route53_acm.tf
@@ -11,6 +11,13 @@ module "acm" {
   tags = {
     Name = "shodapp.seansalmassi.com"
   }
+
+  depends_on = [time_sleep.acm_delay]
+}
+
+# Delay Resource for ACM DNS Validation using time_sleep
+resource "time_sleep" "acm_delay" {
+  create_duration = "60s" # Wait for 60 seconds to allow DNS propagation
 }
 
 # Output for ACM Certificate ARN
@@ -32,10 +39,7 @@ resource "aws_route53_record" "acm_validation" {
   records = [each.value.resource_record_value]
   ttl     = 60
 
-  # Wait for DNS validation by querying Route 53
-  lifecycle {
-    create_before_destroy = true
-  }
+  depends_on = [module.acm, time_sleep.acm_delay]
 }
 
 # Route 53 Record for ALB in Personal Account
@@ -44,31 +48,13 @@ resource "aws_route53_record" "seansalmassi-com" {
 
   zone_id = "Z00891131OSP4IF3CZM29" # Hosted zone ID in the personal account
 
-  name = "shodapp.seansalmassi.com"
-  type = "A"
+  depends_on = [module.acm, time_sleep.acm_delay]
+  name       = "shodapp.seansalmassi.com"
+  type       = "A"
 
   alias {
     name                   = module.alb.lb_dns_name # DNS name of the ALB
     zone_id                = module.alb.lb_zone_id  # Hosted zone ID of the ALB
     evaluate_target_health = true
   }
-}
-
-# Route 53 Validation Record Propagation Check
-data "aws_route53_record" "acm_validation" {
-  provider = aws.personal
-
-  for_each = { for option in module.acm.acm_certificate_domain_validation_options : option.domain_name => option }
-
-  zone_id = "Z00891131OSP4IF3CZM29"
-  name    = each.value.resource_record_name
-  type    = each.value.resource_record_type
-}
-
-# ACM Certificate Validation with DNS Propagation Check
-resource "aws_acm_certificate_validation" "this" {
-  certificate_arn         = module.acm.acm_certificate_arn
-  validation_record_fqdns  = [for option in data.aws_route53_record.acm_validation : option.fqdn]
-
-  depends_on = [aws_route53_record.acm_validation]
 }
